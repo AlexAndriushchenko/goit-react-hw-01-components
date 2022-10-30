0d79d8fbae4e3775f8a3608bfee7e31e77c0e771
AlexAndriushchenko <aleksandr.work3@gmail.com>
2022-10-30 11:13:28 +0000

    add friendsListItem

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,24 +1,21 @@
 import PropTypes from 'prop-types';
 import css from './friends.module.css';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
   return (
     <ul className={css.friend_list}>
-      {friends.map(friend => (
-        <li className={css.item} key={friend.id}>
-          {/* <span className={css.statusOnline} {css.statusOffline}>{friend.isOnline}</span> */}
-          <span
-            className={friend.isOnline ? css.statusOnline : css.statusOffline}
-          ></span>
-          <img
-            className={css.avatar}
-            src={friend.avatar}
-            alt="User avatar"
-            width="48"
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            id={id}
           />
-          <p className={css.name}>{friend.name}</p>
-        </li>
-      ))}
+        );
+      })}
     </ul>
   );
 };
@@ -26,10 +23,10 @@ const FriendList = ({ friends }) => {
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
     })
   ),
 };
